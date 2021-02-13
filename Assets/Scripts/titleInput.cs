00885107d54a104ad3245528dc18de7cb91b5431
kimtaeho <>
2021-02-13 18:33:02 +0900

    3. scene 전환 아무데나 클릭시

---
+++ Assets/Scripts/titleInput.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titleInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+        }
+
+    }
+}
